5abdb372d87c5faa70137ebca2c293bde824e702
nilo <renard.agne@gmail.com>
2016-04-15 01:38:12 -0300

    testes inclusos

--- ubet/tests.py
+++ ubet/tests.py
@@ -3,34 +3,38 @@ from django.test import TestCase
 # Create your tests here.
 from random import sample,randint
 import string
-from ubet.models import User,ubet_user
-import datetime
+from .models import ubet_user,User
+import	 datetime
+from django.utils import timezone
 from django.test.utils import setup_test_environment
 setup_test_environment()
 from django.test import Client
 from django.core.urlresolvers import reverse
 def random_string(arg):
-	return ''.join(random.sample(string.lowercase+string.digits,arg))
-
-def  random_user():
-	ubet_user = ubet_user()
-	ubet_user.full_name = random_string(30)
-	ubet_user.date_of_birth = datetime.date(randint(1900,2000),randint(1,13),randint(1,29))
-	user = User()
-	user.email = random_string(6) + '@' +random_string(6) + '.com'
-	user.password = random_string(10)
-	user.username = random_string(10)
-	user.first_name = random_string(10)
-	ubet_user.django_user = user
-	ubet_user.save()
-	user.save()
-
+	return ''.join(sample(string.lowercase+string.digits,arg))
 
 client = Client()
 class testes(TestCase):
-	def testesignup(self):
-		d = {
-			'teste' : 'camo'
-		}
-		response = client.get('/login/')
-		print response
+	def testaendereco(self):
+		x = ubet_user()
+		x.full_name = random_string(30)
+		db = x.date_of_birth = datetime.date(randint(1900,2000),randint(1,12),randint(1,29))
+		user = User()
+		email = random_string(6) + '@' +random_string(6) + '.com'
+		password = random_string(10)
+		username = user.username = random_string(10)
+		first_name = random_string(10)
+		x.django_user = User.objects.create_user(username,
+			email=email,
+			password=password,
+			first_name=first_name)
+		x.save()
+		self.assertEqual(200,client.get('').status_code)
+		self.assertEqual(200,client.get(reverse('signup')).status_code)
+		self.assertEqual(200,client.get(reverse('login')).status_code)
+		self.assertEqual(200,client.get(reverse('listall')).status_code)
+
+		self.assertNotEqual(None ,User.objects.get(username=username))
+		self.assertEqual(username,ubet_user.objects.get(date_of_birth=db).django_user.username)
+		self.assertEqual(db,User.objects.get(username=username).ubet_user.date_of_birth)
+
